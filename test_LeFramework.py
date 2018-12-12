549e63ae6e292dc9c99e4712a9c3b41d92d4a70b
inXS212 <inXS212@gmail.com>
2018-12-12 13:23:14 +0100

    Adding LeFramework
    yep tho the submodule is not yet included

---
+++ test_LeFramework.py
@@ -0,0 +1,7 @@
+import unittest
+from LeFramework._tests.test_RLReplays import *
+from LeFramework._tests.test_Exercices import *
+from LeFramework._tests.test_Vector import *
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ test_agent.py
@@ -0,0 +1,9 @@
+
+#this helps checking syntax
+
+from LeBaguette.LeBaguette2 import LeBaguette2 as agent
+# from LeBaguette.LeBaguette import Agent
+# from LeCroissant.LeCroissant2 import LeCroissant2 as agent
+# from LeCroissant.LeCroissant import agent
+# from HastamRevenge.HastamBot import HastamBotRevenge as agent
+
